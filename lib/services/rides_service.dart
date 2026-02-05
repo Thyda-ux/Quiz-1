67152b233970c10f2076eaf96e754b50a0e6ec2f
Thyda-ux <sothyda.mao@gmail.com>
2026-02-05 15:45:28 +0700

    BLA_001

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,17 +1,31 @@
 import '../data/dummy_data.dart';
 import '../model/ride/ride.dart';
+import '../model/ride/locations.dart';
 
-////
-///   This service handles:
-///   - The list of available rides
-///
-class RidesService {
-  static List<Ride> allRides = fakeRides;
+class RideFilterService {
+  /// Filter rides by departure, seats
+  static List<Ride> filterRides({
+    required List<Ride> rides,
+    Location? departure,
+    int? minSeats,
+  }) {
+    return rides.where((ride) {
+      // Departure location
+      if (departure != null) {
+        if (!_matchesLocation(ride.departureLocation, departure)) {
+          return false;
+        }
+      }
+      //Available seats
+      if (minSeats != null) {
+        if (ride.remainingSeats < minSeats) {
+          return false;
+        }
+      }  return true;
+    }).toList();
+  }
 
-  // TODO Create a static method to filter rides by departure location
-
-  // TODO Create a static method to filter rides by number of requested seat
-
-  // TODO Create a static method to filter : optional departure location, optional requested seat
-
+  static bool _matchesLocation(Location loc1, Location loc2) {
+    return loc1.name == loc2.name; 
+  }
 }
